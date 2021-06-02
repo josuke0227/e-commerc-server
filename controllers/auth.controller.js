cdf11a8c1a6b677458bdefda5e1d3028f15f44ab
Josuke <y.motosugi0227@gmail.com>
2021-06-02 17:35:16 +1000

    Built route to get current user.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,18 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.currentUser = async (req, res) => {
-  User.findOne({ email: req.user.email }).exec((err, user) => {
-    if (err) throw new Error(err);
-    res.json(user);
-  });
+  const token = req.headers["x-auth-token"];
+
+  try {
+    const jwtSecretKey = process.env.JWT_SECRET;
+    const { email } = jwt.verify(token, jwtSecretKey);
+
+    User.findOne({ email }).exec((err, user) => {
+      if (err) throw new Error(err);
+      res.json(user);
+    });
+  } catch (error) {
+    res.status(400).send("Invalid token.");
+  }
 };
