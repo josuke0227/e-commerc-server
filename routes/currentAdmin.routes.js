b7cf9985cf95134b3c08958745e3b17f72928fec
Josuke <y.motosugi0227@gmail.com>
2021-06-03 08:49:06 +1000

    Delete route to get current user.

--- routes/currentAdmin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { currentUser } = require("../controllers/auth.controller");
-const adminRoute = require("../middleware/adminRoute");
-const router = express.Router();
-
-router.post("/", adminRoute, currentUser);
-
-module.exports = router;
