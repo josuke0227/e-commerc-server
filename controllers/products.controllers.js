20ae7fee03322e70d3250e68ebbe63a4ab8b0a9f
Josuke <y.motosugi0227@gmail.com>
2021-06-12 13:57:50 +1000

    Add response status

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -5,7 +5,7 @@ exports.create = async (req, res) => {
   try {
     req.body.slug = slugify(req.body.title);
     const newProduct = await new Product(req.body).save();
-    res.json(newProduct);
+    res.status(200).json(newProduct);
   } catch (error) {
     console.log("Product create error", error);
     res.status(400).json({
