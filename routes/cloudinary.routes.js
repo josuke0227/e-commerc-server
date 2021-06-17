2bc56ef596f3690ad367d0ef13ec54e9ebb021dd
Josuke <y.motosugi0227@gmail.com>
2021-06-17 11:06:12 +1000

    Add route which return image by prodcut id.

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const adminRoute = require("../middleware/adminRoute");
 
-const { upload, remove } = require("../controllers/cloudinary.contollers");
+const {
+  upload,
+  remove,
+  images,
+} = require("../controllers/cloudinary.contollers");
 
 router.post("/upload", adminRoute, upload);
 router.post("/remove", adminRoute, remove);
+router.get("/:productId", adminRoute, images);
 
 module.exports = router;
